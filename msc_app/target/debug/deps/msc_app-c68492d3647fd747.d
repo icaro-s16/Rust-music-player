19ba413962e6be85126b53e4135f50b0a8820c70
Ikr <santanaikaru@gmail.com>
2026-04-28 08:16:38 -0300

    Delete msc_app/target directory

--- msc_app/target/debug/deps/msc_app-c68492d3647fd747.d
+++
@@ -1,7 +0,0 @@
-/home/icaro_s/projetos/rubber_duck_msc_player/msc_app/target/debug/deps/msc_app-c68492d3647fd747.d: src/lib.rs src/app.rs src/ui.rs
-
-/home/icaro_s/projetos/rubber_duck_msc_player/msc_app/target/debug/deps/msc_app-c68492d3647fd747: src/lib.rs src/app.rs src/ui.rs
-
-src/lib.rs:
-src/app.rs:
-src/ui.rs:
